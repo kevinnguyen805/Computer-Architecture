10da58d2ce2d42e7be28341d150f5b58a4b1ec1b
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-03-27 09:59:10 -0700

    mvp complete sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,15 +6,26 @@ LDI = 0b10000010
 PRN = 0b01000111 
 HLT = 0b00000001
 MUL = 0b10100010
+
 # Stack
 PUSH = 0b01000101   # push the value in the given register on the stack
 POP = 0b01000110    # pop the value at the top of the register (stack) 
-
 CALL = 0b01010000
 RET = 0b00010001
 ADD = 0b10100000
+SP = 7  # for stack methods
 
-SP = 7 #for stack methods 
+# Sprint challenge 
+CMP = 0b10100111
+JMP = 0b01010100 
+JEQ = 0b01010101
+JNE = 0b01010110 
+# Flags for CMP less than / greater than / equal to 
+LT = 0b00000100
+GT = 0b00000010
+EQ = 0b00000001
+
+
 
 
 class CPU:
@@ -26,7 +37,8 @@ class CPU:
         # construct RAM + REG + PC 
         self.ram = [0] * 256
         self.reg = [0] * 8
-        self.pc = 0
+        self.pc = 0       # program counter is the address of currently executing instructions 
+        self.fl = 0
         self.bt = {       # branch table for code beautification 
             LDI: self.op_ldi,
             PRN: self.op_prn,
@@ -37,6 +49,10 @@ class CPU:
             PUSH: self.op_push,
             CALL: self.op_call,
             RET: self.op_ret,
+            CMP: self.op_cmp, 
+            JMP: self.op_jmp,
+            JEQ: self.op_jeq,  
+            JNE: self.op_jne 
         }
 
     def op_ldi(self, operand_a, operand_b):
@@ -67,7 +83,24 @@ class CPU:
     def op_ret(self):
         self.pc = self.ram[self.reg[SP]]
         self.reg[SP] += 1
-
+    def op_cmp(self, operand_a, operand_b):
+        self.alu("CMP", operand_a, operand_b)
+    def op_jmp(self, operand_a, operand_b):
+        # set the PC to the address stored in the given register 
+        self.pc = self.reg[operand_a]
+    def op_jeq(self, operand_a, operand_b):
+        # if equal flag is set (true), jump to the address stored in the given register
+        if self.fl == EQ: 
+            self.pc = self.reg[operand_a]
+        else: 
+            self.pc += 2
+    def op_jne(self, operand_a, operand_b):
+        # if equal flag is clear (false, 0) jump to the address stored in the given register
+        if not self.fl == EQ: 
+            self.pc = self.reg[operand_a]
+        else: 
+            self.pc += 2
+    
 
 
 
@@ -133,6 +166,13 @@ class CPU:
         ## ADDING MULTIPLICATION
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = LT 
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = GT 
+            else: 
+                self.fl = EQ
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -144,7 +184,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
+            self.fl,
             #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -165,7 +205,8 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             instruction_length = (instruction >> 6) + 1
-            instruction_set_pc = (instruction >> 4) == 1
+            instruction_set_pc = ((instruction >> 4) & 0b1 )== 1
+            
             if instruction in self.bt:
                 self.bt[instruction](operand_a, operand_b)
             #check if opcode instructions sets the pc
